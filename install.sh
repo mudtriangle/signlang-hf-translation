565b106205225e6c691a7eb786732d871beb1a77
Jose Marcelo Sandoval-Castaneda <marcelo@gpu31.ttic.edu>
2023-11-24 14:28:21 -0600

    Minor fix with install

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ conda create -n signlang-hf python=3.8.1 -y
 conda activate signlang-hf
 conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia -y
 pip install jsonargparse
-pip install transformers
+pip install transformers==4.30.2
+pip install accelerate -U
 pip install evaluate
+pip install jiwer
 
